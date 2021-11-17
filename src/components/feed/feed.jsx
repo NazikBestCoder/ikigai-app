0aaea7d2794573ae156a630b962e97fb7a1c9080
NazikBest Coder <n.askarbekkyzy@gmail.com>
2021-11-17 12:11:17 -0500

    share.jsx

--- src/components/feed/feed.jsx
+++ src/components/feed/feed.jsx
@@ -1,7 +1,17 @@
-import "./feed.css"
+import Post from "../post/Post";
+import Share from "../share/Share";
+import "./feed.css";
+import { Posts } from "../../dummyData";
+
 export default function Feed() {
     return (
         <div className="feed">
-            <div className="feedWrapper"</div>
-    )
+            <div className="feedWrapper">
+        <Share />
+        {Posts.map((p) => (
+          <Post key={p.id} post={p} />
+        ))}
+      </div>
+    </div>
+  );
 }
