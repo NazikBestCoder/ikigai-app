be30a38d22605d9cb47af63c0b6e9b48e9b63466
NazikBest Coder <n.askarbekkyzy@gmail.com>
2021-11-18 11:58:19 -0500

    yarn

---
+++ ikigai-app/api/models/User.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose")
+
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        trim:true,
+        required:true,
+        unique:true,
+        min:2,
+        max:20
+    },
+    email: {
+        type:String,
+        trim:true,
+        required:true,
+        unique:true,
+        lowercase:true,
+        max:50
+    },
+    password: {
+        type:String,
+        required:true,
+        trim:true,
+        min:6
+    },
+    profilePicture: {
+        type:String,
+        default: ""
+    },
+    coverPicture: {
+        type:String,
+        default: ""
+    },
+    followers: {
+        type:Array,
+        default:[]
+    },
+    followings: {
+        type:Array,
+        default:[]
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    desc: {
+        type:String,
+        max: 50
+    },
+    city: {
+        type:String,
+        max:50
+    },
+    from: {
+        type:String,
+        max:50
+    },
+    relationship: {
+        type: Number,
+        enum:[1,2,3]
+    },
+}, {timestamps:true})
+
+
+module.exports = mongoose.model("User", UserSchema)
